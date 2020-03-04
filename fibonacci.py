2b17cb5e15dbcdc904c538b47dfd967f10aa8cd8
Oğuz Balkaya <balkayablky58@gmail.com>
2020-03-04 21:06:54 +0300

    İstenen fibonacci sayısını bulan fonksiyon

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,7 @@
 #n. fibonacci sayısını bulur.
 known={0:0,1:1}
 def fibo_rec(n):
- #   if(n<2):
-  #      return n
-   # else:
-    #    return fibo_rec(n-1)+fibo_rec(n-2)
     if n in known:
-        #print(known)
         return known[n]
     else:
         result = fibo_rec(n-1)+fibo_rec(n-2)
