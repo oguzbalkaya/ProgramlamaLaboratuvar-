217f022c9c4e03cd7c5c681c6dc5a8b023ac8987
Oğuz Balkaya <balkayablky58@gmail.com>
2020-03-06 13:23:04 +0300

    Recursive üs alma

---
+++ recursiveusalma.py
@@ -0,0 +1,13 @@
+#Recursive olarak üs alır.
+def power(a,b):
+    if b==0:
+        return 1
+    elif b==1:
+        return a
+    else:
+        if b%2 == 0:
+            return power(a*a,b//2)
+        else:
+            return power(a*a,b//2)*a
+
+print(power(3,15))
